df7288218412ddc643d75bd11108f9af381e30e6
Kf <kf_ke@hikingbook.net>
2023-03-31 15:30:40 +0800

    Nullity check

--- src/person.js
+++ src/person.js
@@ -4,6 +4,8 @@ export default class Person {
   constructor(object) {
     this.name = object.name;
     this.email = object.emil;
-    this.link = new Link(object.link);
+    if (object.link) {
+      this.link = new Link(object.link);
+    }
   }
 }
